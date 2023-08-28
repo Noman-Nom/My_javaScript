8e47d99b8da68fd28bfb8620bbc749fa753540e4
Noman <noman5456318@gmail.com>
2023-08-28 10:47:04 +0500

    new file added

---
+++ 10_objects/script.js
@@ -0,0 +1,21 @@
+const tinderUser={}
+
+
+tinderUser.id = "123abc"
+tinderUser.name = "Noman"
+tinderUser.email = "noman@gmail.com"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);s
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname)
