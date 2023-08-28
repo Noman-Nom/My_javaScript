1c349bd27bf0b6b85cbc17e6384539ddb50f57e2
Noman <noman5456318@gmail.com>
2023-08-28 11:03:03 +0500

    file updated

--- 10_objects/script.js
+++ 10_objects/script.js
@@ -19,3 +19,34 @@ const regularUser = {
 }
 
 // console.log(regularUser.fullname.userfullname)
+
+// Addition of two objects 
+
+const object1 = {
+    1: "a",
+    2: "b"
+}
+const object2 = {
+    3: "c",
+    4: "d"
+}
+const object3 = {
+    5: "e",
+    6: "f"
+}
+
+//  method 1 jis meen ak object ak andar dosra object ata he which is not good 
+// const object4 = {object1 , object2 , object3}
+// console.log(object4)
+
+// method 2  efficient and useful method 
+
+// const object4 = {...object1, ...object2, ...object3}
+// console.log(object4)
+
+
+// more use full methods and techniques of objects 
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser)) 
+
