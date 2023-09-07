2b95876e4f62115f6469444d18151369c712f09c
Noman <noman5456318@gmail.com>
2023-09-07 12:22:15 +0500

    file added

---
+++ 16_for-loop/script.js
@@ -0,0 +1,33 @@
+// basic program 
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(i)
+// }
+
+
+// for loop  with if
+
+// for (let i = 0; i <= 10; i++) {
+
+//     if(i===5){
+//         console.log("3 is my fvt number")
+//     }
+//     const element = i;
+
+//     if (element === 9) {
+//         console.log("9 is also my fvt number")
+
+//     }
+
+//     console.log(element)
+
+
+// }
+
+
+// 
+
+
+
+
