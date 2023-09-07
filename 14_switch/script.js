b39354354a3a4602f88604b2e024789c09b529f9
Noman <noman5456318@gmail.com>
2023-09-07 11:13:56 +0500

    file added

---
+++ 14_switch/script.js
@@ -0,0 +1,23 @@
+
+// switch cases in js 
+// it is more effiecient then if else statement 
+
+const month = 4;
+
+switch (month) {
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("April")
+        break;
+
+    default:
+        break;
+}
