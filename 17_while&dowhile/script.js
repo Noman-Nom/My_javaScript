d5637c7fb73e9b42daafef4c8826c6ad24016ebb
Noman <noman5456318@gmail.com>
2023-09-07 13:16:57 +0500

    file added

---
+++ 17_while&dowhile/script.js
@@ -0,0 +1,19 @@
+//  while loop  in js 
+
+// let index = 0
+
+// while (index <=10) {
+//     console.log(`the value of in index = ${index}`)
+//     index +=1
+// }
+
+//  while loop with arry 
+
+const myArr = ['noman' , 'ibrahim' , 'raza']
+
+let arr = 0;
+while(arr<=myArr.length){
+    console.log(myArr[arr])
+    arr +=1
+
+}
