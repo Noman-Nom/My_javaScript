8216234b06ec57fe59a9af8c13f21d06238ec697
Noman <noman5456318@gmail.com>
2023-09-07 15:20:27 +0500

    file added

--- 18_high-order-arr-loop/script.js
+++ 18_high-order-arr-loop/script.js
@@ -12,14 +12,28 @@
 //     console.log(`char of greet are,  ${greets}`)
     
 // }
-const greetings = "Helo world"
+// const greetings = "Helo world"
 
-for (const greets of greetings) {
-if (greets == " ") {
-    continue
-}
- else{
-    console.log(`char of greet are,  ${greets}`)
- }
+// for (const greets of greetings) {
+// if (greets == " ") {
+//     continue
+// }
+//  else{
+//     console.log(`char of greet are,  ${greets}`)
+//  }
     
+// }
+
+
+const map = new Map()
+
+map.set('pk', 'pakistan' )
+map.set('in', 'india' )
+map.set('dxb', 'dubai' )
+ 
+for (const [key,value] of map) {
+    console.log(key , ';-', value)
 }
+
+
+// object par for of loop nahi laga sakte hen us k lie for in lagaya jata he 
