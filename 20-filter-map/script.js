bd5addc82bdbaa9c18730347d19c9b58caa5f313
Noman <noman5456318@gmail.com>
2023-09-09 12:57:58 +0500

    new file added

--- 20-filter-map/script.js
+++ 20-filter-map/script.js
@@ -32,4 +32,32 @@ myNums.forEach((num)=>{
         }
 }) 
 
-console.log(newNums)
+// console.log(newNums)
+
+//  database se isi trha ka data ata he
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+//   const userBooks = books.filter((bk)=>{
+//             return bk
+//   })
+//   const userBooks = books.filter((bk)=>{
+//             return bk.genre === 'History'
+//   })
+  const userBooks = books.filter((bk)=> 
+  
+  {
+      return bk.publish>=1987 && bk.genre === 'Science'  && bk.edition >= 2016
+} )
+console.log(userBooks)
+
