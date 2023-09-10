020e771be3acafffc046edc7aeec6244e72487be
Noman <noman5456318@gmail.com>
2023-09-11 00:42:41 +0500

    new changes

--- Dom-project1/script.js
+++ Dom-project1/script.js
@@ -12,9 +12,17 @@ div.forEach((div) => {
     if (e.target.id === "blue") body.style.backgroundColor = "#2F80ED";
     if (e.target.id === "black") body.style.backgroundColor = "#20002c";
     if (e.target.id === "green") body.style.backgroundColor = "#0f9b0f";
+    if (e.target.id === "violet") body.style.backgroundColor = e.target.id;
+    if (e.target.id === "brown") body.style.backgroundColor = e.target.id;
+    if (e.target.id === "purple") body.style.backgroundColor = e.target.id;
+    if (e.target.id === "skyblue") body.style.backgroundColor = e.target.id;
   });
 });
 
 
 //  method 2 
+
+// const div = document.querySelectorAll('.colors')
+// const div = document.getElementById('blue')
+// console.log(div)
 
