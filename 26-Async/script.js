de456c8a8cbe77b8e980788e635eda44ff687efe
Noman <noman5456318@gmail.com>
2023-09-13 14:58:57 +0500

    file completed

---
+++ 26-Async/script.js
@@ -0,0 +1,31 @@
+// set timeout -- itne time k bad is kam ko karna he 
+
+
+// ----method 1 
+
+setTimeout(function(){
+        // console.log("Muhammad Noman")
+},2000)
+
+// method 2 
+const PrintName = ()=>{
+    // console.log('NOMAN')
+}
+setTimeout(PrintName ,2000)
+
+// 
+const container = document.querySelector('#container')
+const sayHi = setTimeout(()=>{
+        container.innerHTML ="Hello Muhammad Noman"
+},2000)
+
+//  if we want to stop this 
+
+// clearTimeout(sayHi) 
+//  not efficient 
+
+ document.querySelector('#btn')
+ .addEventListener( "click",function(){
+    clearTimeout(sayHi)
+    console.log("Stop")
+ })
