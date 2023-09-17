772b29f916a657f8f8009dc069f153f7ede056e7
Noman <noman5456318@gmail.com>
2023-09-17 21:32:23 +0500

    promises with Api completed

--- promises/script.js
+++ promises/script.js
@@ -102,10 +102,10 @@ setTimeout(()=>{
 async function consumePromiseFive(){
     try {
         const response = await promise5
-        console.log(response)
+        // console.log(response)
         
     } catch (error) {
-        console.log(error)
+        // console.log(error)
         
     }
 }
@@ -116,3 +116,42 @@ async function consumePromiseFive(){
 //     console.log(response)
 // }
 consumePromiseFive()
+
+
+// with API
+
+
+// async function getUser(){
+//        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//        const data = await response.json()
+//        console.log(data)
+// }
+
+async function getUser(){
+       try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+       const data = await response.json()
+    //    console.log(data)
+       } catch (error) {
+        // console.log("E: eror")
+       }
+}
+
+getUser()
+
+//  with then and catch
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+        return response.json()
+
+})
+.then((data)=>{
+    console.log(data)
+
+})
+.catch((error)=>{
+console.log(error)
+})
